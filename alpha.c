48f86b333e89409f791c7f035acbe0e5534dc146
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2017-08-05 16:59:44 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+clrscr();
+printf("\n enter number or alphabet");
+scanf("%s",&n);
+if(n='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
