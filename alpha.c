a6240961106a838dda9e045a3ce6eaee2d5a092f
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2017-08-08 15:50:16 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -6,4 +6,13 @@ char n;
 clrscr();
 printf("\n enter number or alphabet");
 scanf("%s",&n);
-if(n='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+if(n='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+{
+  printf("\n the character is a alphabet");
+}
+  else
+  {
+    printf("\n the character is a number");
+  }
+  getch();
+}
