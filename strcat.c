ecf0413ca1e7bf0462bca5163f7357b53d479237
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-02 15:57:33 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char str1[30],str2[30];
+	printf("\n enter the first string:");
+	scanf("%s",&str1);
+	printf("\n enter the second string:");
+	scanf("%s",&str2);
+	printf("\n concatenated string is:%s%s",str1,str2);
+	return 0;
+}
