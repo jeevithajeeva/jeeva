4dca278ef623c7162d0fb35f2125d9405d315b7a
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-02 16:10:54 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("\n enter the number");
+	scanf("%d",&n);
+	for(i=1;i<=10;i++)
+	{
+		if(i==n)
+		{
+			printf("\n yes");
+			break;
+		}
+	}
+	if(i!=n)
+	printf("\n no");
+	return 0;
+}
