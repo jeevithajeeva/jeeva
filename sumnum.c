f44c8f0e24f27f7c52d77f00f976ceafef38ea7b
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-05 16:31:22 +0530

    Create sumnum.c

---
+++ sumnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int sum=0,n,rem;
+  printf("\n enter a number");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    rem=n%10;
+    sum=sum+rem;
+    n=n/10;
+  }
+  printf("%d",sum);
+  return 0;
+}
