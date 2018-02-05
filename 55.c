17d11c3581940c05354a61495ed5b19db2b7caec
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-05 16:47:11 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("\n enter two numbers:");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	if(c%2==0)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
