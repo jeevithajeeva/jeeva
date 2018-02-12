7aec37044ddf4038553bbd40d285c22e872e56e2
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-12 16:46:39 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -6,7 +6,7 @@ char n;
 clrscr();
 printf("\n enter number or alphabet");
 scanf("%s",&n);
-if(n='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+if(n>='a'&&n<='z')
 {
   printf("\n the character is a alphabet");
 }
