c7739b623bfca83da3b582afcc3ce7f46aee9e42
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-12 15:49:04 +0530

    Rename primenum.c to pri.c

---
+++ pri.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int num,i,prime=0;
+	printf("\n enter the number:");
+	scanf("%d",&num);
+	for(i=2;i<=num/2;++i)
+	{
+	if(num%i==0)
+	{
+		prime=1;
+		break;
+	}
+	}
+	if(prime==1)
+	{
+		printf("\n %d is not a prime number:");
+	}
+	else
+	{
+		printf("\n %d is a prime number:");
+	}
+	return 0;
+}
