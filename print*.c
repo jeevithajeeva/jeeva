86f40c70dff22c22d1231b20ff2af44c6933afc5
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-19 16:50:43 +0530

    Create print*.c

---
+++ print*.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[50];
+	int len;
+	printf("\n enter the string");
+	scanf("%s",&str);
+	len=strlen(str);
+	if(len%2==0)
+	{
+		str[len/2]='*';
+		str[len/2-1]='*';
+	}
+	else
+	{
+		str[len/2]='*';
+	}
+	printf("\n%s",str);
+	return 0;
+}
