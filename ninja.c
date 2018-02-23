568de9d7bef3582b6ec081569de68f6279deba60
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-02-23 16:13:31 +0530

    Update ninja.c

--- ninja.c
+++ ninja.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
+	int a,b,c,i;
 	printf("\n enter the two numbers:");
-	scanf("%d%d",&a,&b);
+	for(i=0;i<3;i++)
+	{
+	scanf("%d %d",&a,&b);
+	}
 	if(a>b)
 	{
 		c=a-b;
