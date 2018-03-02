9dbfb520446d9e4656bf39cf68d677b87623cb41
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-02 16:48:30 +0530

    Update alphanum.c

--- alphanum.c
+++ alphanum.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include<string.h>
 int main()
 {
-    char str[100],count;
-    int i;
+    char str[100];
+    int i,n,count=0;
     printf("Enter any string : ");
     scanf("%s",&str);
+    n=strlen(str);
     for(i=0;str[i]!='\0';i++)
     {
-        if((str[i]>='a' && str[i]<='z') || (str[i]>='0' && str[i]<='9'))
+        if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
         {
          count=1;
         }
-        else 
+        else if(str[i]>='0' && str[i]<= '9')
         {
         	count=2;
         }
+        else
+        {
+            count=0;
+        }
     }
-    if(count==1)
+    if(count==1 && count==2)
        {
        	printf("\n yes");
        }
