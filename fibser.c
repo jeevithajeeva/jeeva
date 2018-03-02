3ded98d9c14b2af5db92976adfd3e49440840027
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-02 16:56:28 +0530

    Update fibser.c

--- fibser.c
+++ fibser.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int x=0,y=1,z,n,i;
+	int x=0,y=1,z=1,n,i;
 	printf("\n enter the number of terms");
 	scanf("%d",&n);
 	printf("\n the fibonacci series is");
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
-	printf("\n%d",x);
+	printf("\n%d",z);
 	z=x+y;
 	x=y;
 	y=z;
