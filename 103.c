cdf12190e16db7891fcc8a9597fa79db352fbe0f
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-05 16:34:25 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+	char str[50];
+	int i;
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(i==0)
+		{
+			str[i]=toupper(str[i]);
+		}
+		if(str[i]==' ')
+		{
+			str[i+1]=toupper(str[i+1]);
+		}
+	}
+	printf("\n%s",str);
+	return 0;
+}
