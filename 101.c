8bbbda3582e491cccabd3e4386d8dc2f16cb8b37
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-05 15:59:03 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[20];
+	int i,length,n;
+	printf("\n enter the string");
+	scanf("%s",&str);
+	printf("\n enter the number:\n");
+	scanf("%d",&n);
+	length=strlen(str);
+	for(i=n;i<=length;i++)
+	{
+		printf("%c",str[i]);
+	}
+	return 0;
+}
