78104638869b1c944146eb4cba96cb7e5c02c446
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-09 15:37:14 +0530

    Update 112.c

--- 112.c
+++ 112.c
@@ -4,13 +4,14 @@ int main()
 	int n,k,i,a[50],count=0;
 	printf("\n enter the number:");
 	scanf("%d",&n);
+	printf("\n enter number to search");
+	scanf("%d",&k);
 	printf("\n enter n numbers:");
 	for(i=1;i<=n;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	printf("\n enter number to search");
-	scanf("%d",&k);
+	
 	for(i=0;i<n;i++)
 	{
 		if(a[i]==k)
