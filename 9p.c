7010529a51677d53de9c3aeea5dc137f79b5dd4b
jeevithajeeva <30549675+jeevithajeeva@users.noreply.github.com>
2018-03-16 16:00:42 +0530

    Create 9p.c

---
+++ 9p.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int l,r,i,count=0,j,mid,f;
+	printf("\n enter the range:");
+	scanf("%d%d",&l,&r);
+	mid=(l+r)/2;
+	for(i=l;i<=r;i++)
+	{
+		for(j=2;j<i;j++)
+		{
+		  if(i%j==0)
+		   {
+			f=1;
+			break;
+	  	   }
+	  	   else
+	  	   {
+	  	   	f=0;
+	  	   }
+	
+		}
+	if(f==0)
+	{
+	count=count+1;
+	}
+	}
+	printf("\n%d",count);
+	return 0;
+}
